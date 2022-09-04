c61b967e944f82a4b5626072cbbd9c4cb21e58cf
Khoa, Le <leminkhoa@gmail.com>
2022-09-04 15:25:51 +0700

    update basic_cleaning to include longitude and latitude filter

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,11 @@ def go(args):
     logger.info("Start converting last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Drop rows in the dataset that are not in the proper geolocation.
+    logger.info("Start filtering longitude and latitude")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
 
